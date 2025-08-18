a18f2d7100964cb398bdde3ad2bb77c9492fd1e9
FranchelysBatista <211575755+franchelysBatistaM@users.noreply.github.com>
2025-08-17 22:01:27 -0400

    creando infrastructure interfaces IActividadRepository

---
+++ project/ProjectFinal/ActividadesDeportivas.Infrastructure/Interfaces/IActividadRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using ActividadesDeportivas.Domain.Entities;
+
+namespace ActividadesDeportivas.Infrastructure.Interfaces
+{
+    public interface IActividadRepository : IGenericRepository<ActividadFisica>
+    {
+        Task<IEnumerable<ActividadFisica>> GetByUsuarioAsync(int usuarioId);
+    }
+}
