4e3fa9f531efac6f25abc4d34693692bbd714cfd
FranchelysBatista <211575755+franchelysBatistaM@users.noreply.github.com>
2025-08-17 22:02:25 -0400

    creando infrastructure interfaces IResumenFitbitRepository

---
+++ project/ProjectFinal/ActividadesDeportivas.Infrastructure/Interfaces/IResumenFitbitRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+using ActividadesDeportivas.Domain.Entities;
+
+namespace ActividadesDeportivas.Infrastructure.Interfaces
+{
+    public interface IResumenFitbitRepository : IGenericRepository<ResumenDiarioFitbit>
+    {
+        Task<ResumenDiarioFitbit?> GetByUsuarioAndFechaAsync(int usuarioId, DateTime fecha);
+    }
+}
