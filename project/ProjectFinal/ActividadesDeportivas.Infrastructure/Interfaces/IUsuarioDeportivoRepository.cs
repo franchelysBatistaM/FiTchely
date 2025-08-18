8f1742c27799d647ca0b9f677fbf1af0c479d670
FranchelysBatista <211575755+franchelysBatistaM@users.noreply.github.com>
2025-08-17 22:05:12 -0400

    creando infrastructure IUsuarioDeportivoRepository

---
+++ project/ProjectFinal/ActividadesDeportivas.Infrastructure/Interfaces/IUsuarioDeportivoRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ActividadesDeportivas.Domain.Entities;
+
+namespace ActividadesDeportivas.Infrastructure.Interfaces
+{
+    public interface IUsuarioDeportivoRepository
+    {
+        Task<UsuarioDeportivo?> GetByIdAsync(int id);
+        Task<UsuarioDeportivo?> GetByEmailAsync(string email);
+        Task<bool> ExistsByEmailAsync(string email);
+        Task AddAsync(UsuarioDeportivo usuario);
+        Task UpdateAsync(UsuarioDeportivo usuario);
+        Task DeleteAsync(UsuarioDeportivo usuario);
+    }
+}
