133f6187f96016ef3900d063e8ba3f393efc6b9a
FranchelysBatista <211575755+franchelysBatistaM@users.noreply.github.com>
2025-08-17 21:24:53 -0400

    creando interfaces\infrastructure\estadistica

---
+++ project/ProjectFinal/ActividadesDeportivas.Infrastructure/Interfaces/IEstadisticaProgresoRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ActividadesDeportivas.Domain.Entities;
+
+namespace ActividadesDeportivas.Infrastructure.Interfaces
+{
+    public interface IEstadisticaProgresoRepository : IGenericRepository<EstadisticaProgreso>
+    {
+        Task<IEnumerable<EstadisticaProgreso>> GetByUsuarioAsync(int usuarioId);
+    }
+}
