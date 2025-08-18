0437f15ca6a9ba4cd6cbd16fc5ea3ab4aa0b7dd1
FranchelysBatista <211575755+franchelysBatistaM@users.noreply.github.com>
2025-08-17 21:14:09 -0400

    Setting Api

---
+++ project/ProjectFinal/ActividadesDeportivas.Api/Settings/FitbitSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace ActividadesDeportivas.Api.Settings
+{
+    public class FitbitSettings
+    {
+        public string ClientId { get; set; } = string.Empty;
+        public string ClientSecret { get; set; } = string.Empty;
+        public string RedirectUri { get; set; } = string.Empty;
+    }
+}
