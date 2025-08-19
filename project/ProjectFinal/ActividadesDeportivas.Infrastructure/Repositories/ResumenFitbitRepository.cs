667a64553af973873c269ce2c7bc00cff2ac5dc7
FranchelysBatista <211575755+franchelysBatistaM@users.noreply.github.com>
2025-08-19 15:36:22 -0400

    creando ResumenFitbitRepository

---
+++ project/ProjectFinal/ActividadesDeportivas.Infrastructure/Repositories/ResumenFitbitRepository.cs
@@ -0,0 +1,26 @@
+﻿using ActividadesDeportivas.Domain.Entities;
+using ActividadesDeportivas.Infrastructure.Context;
+using ActividadesDeportivas.Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ActividadesDeportivas.Infrastructure.Repositories
+{
+    public class ResumenFitbitRepository : GenericRepository<ResumenDiarioFitbit>, IResumenFitbitRepository
+    {
+
+        public ResumenFitbitRepository(ActividadesDbContext context) : base(context)
+        {
+
+        }
+
+        public async Task<ResumenDiarioFitbit?> GetByUsuarioAndFechaAsync(int usuarioId, DateTime fecha)
+        {
+            return await _context.ResumenesDiariosFitbit
+                                 .Where(r => r.UsuarioDeportivoId == usuarioId && r.Fecha.Date == fecha.Date)
+                                 .FirstOrDefaultAsync();
+        }
+    }
+}
