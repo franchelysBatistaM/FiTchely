f29879c558e6d349610f22fd0e3c14b6ca126e2e
FranchelysBatista <211575755+franchelysBatistaM@users.noreply.github.com>
2025-08-19 15:36:49 -0400

    creando EstadisticaProgresoRepository

---
+++ project/ProjectFinal/ActividadesDeportivas.Infrastructure/Repositories/EstadisticaProgresoRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ActividadesDeportivas.Domain.Entities;
+using ActividadesDeportivas.Infrastructure.Context;
+using ActividadesDeportivas.Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ActividadesDeportivas.Infrastructure.Repositories
+{
+    public class EstadisticaProgresoRepository : GenericRepository<EstadisticaProgreso>, IEstadisticaProgresoRepository
+    {
+        private readonly ActividadesDbContext _db;
+
+        public EstadisticaProgresoRepository(ActividadesDbContext context) : base(context)
+        {
+            _db = context;
+        }
+
+        public async Task<IEnumerable<EstadisticaProgreso>> GetByUsuarioAsync(int usuarioId)
+        {
+            return await _db.EstadisticasProgreso
+                .Where(e => e.UsuarioDeportivoId == usuarioId && e.Activo)
+                .OrderByDescending(e => e.Fecha)
+                .ToListAsync();
+        }
+    }
+}
