dd6766b7b97bcf5603af2ddbca08abc8a0eb6462
FranchelysBatista <211575755+franchelysBatistaM@users.noreply.github.com>
2025-08-19 15:37:12 -0400

    creando ActividadRepository

---
+++ project/ProjectFinal/ActividadesDeportivas.Infrastructure/Repositories/ActividadRepository.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ActividadesDeportivas.Domain.Entities;
+using ActividadesDeportivas.Infrastructure.Context;
+using ActividadesDeportivas.Infrastructure.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace ActividadesDeportivas.Infrastructure.Repositories
+{
+    public class ActividadRepository : GenericRepository<ActividadFisica>, IActividadRepository
+    {
+        private readonly ActividadesDbContext _db;
+
+        public ActividadRepository(ActividadesDbContext context) : base(context)
+        {
+            _db = context;
+        }
+
+        public async Task<IEnumerable<ActividadFisica>> GetByUsuarioAsync(int usuarioId)
+        {
+            return await _db.ActividadesFisicas
+                .Where(a => a.UsuarioDeportivoId == usuarioId && a.Activo)
+                .ToListAsync();
+        }
+    }
+}
